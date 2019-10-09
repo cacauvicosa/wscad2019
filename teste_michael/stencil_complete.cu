17099cdcc2133e51b284807eb16f039f0487f7ac
canesche <michael.canesche@gmail.com>
2019-10-09 13:20:42 -0300

    update

--- teste_michael/stencil_complete.cu
+++ teste_michael/stencil_complete.cu
@@ -1,3 +1,64 @@
+#include <stdio.h>
+#include <cuda_runtime_api.h>
+
+#define SIZE 27
+#define RADIUS 3
+#define GRID_SIZE 512
+#define BLOCK_SIZE 256
+
+__global__ void stencil_1d(int *in, int *out, int dim) {
+
+    __shared__ int temp[BLOCK_SIZE + 2*RADIUS];
+
+  int lindex = threadIdx.x + RADIUS;
+  int gindex = threadIdx.x + blockDim.x * blockIdx.x;
+  int stride = gridDim.x * blockDim.x;
+  int left, right;
+
+  // Go through all data
+  // Step all threads in a block to avoid synchronization problem
+  while ( gindex < (dim + blockDim.x) ) {
+
+    // Read input elements into shared memory
+    temp[lindex] = 0;
+    if (gindex < dim) 
+      temp[lindex] = in[gindex];
+
+    // Populate halos, set to zero if we are at the boundary
+    if (threadIdx.x < RADIUS) {
+
+      temp[lindex - RADIUS] = 0;
+      left = gindex - RADIUS;
+      if (left >= 0)
+        temp[lindex - RADIUS] = in[left];
+
+      temp[lindex + blockDim.x] = 0;
+      right = gindex + blockDim.x;
+      if (right < dim)
+        temp[lindex + blockDim.x] = in[right];
+    }
+
+    // Synchronize threads - make sure all data is available!
+    __syncthreads();
+
+    // Apply the stencil
+    int result = 0;
+    for (int offset = -RADIUS; offset <= RADIUS; offset++) {
+      result += temp[lindex + offset];
+    }
+
+    // Store the result
+    if (gindex < dim)
+      out[gindex] = result;
+
+    // Update global index and quit if we are done
+    gindex += stride;
+
+    __syncthreads();
+
+  }
+}
+
 void verify(int *h_in, int *h_out, int N) {
     int i, j, ij, result, err;
     // check results
